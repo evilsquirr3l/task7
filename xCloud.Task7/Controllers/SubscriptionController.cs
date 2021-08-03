e14ec23b1061d13336e6c901dc0d6033779fde49
Serhii Denshchyk <serhii_denshchyk@epam.com>
2021-08-03 19:11:19 +0300

    feat: add email unsubscribe endpoint

--- xCloud.Task7/Controllers/SubscriptionController.cs
+++ xCloud.Task7/Controllers/SubscriptionController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Amazon.SimpleNotificationService.Model;
 using Microsoft.AspNetCore.Mvc;
@@ -29,6 +30,32 @@ namespace xCloud.Task7.Controllers
             var subscribeResponse = await snsClient.SubscribeAsync(subscribeRequest);
 
             return View(subscribeResponse.ResponseMetadata);
+        }
+        
+        [HttpGet]
+        public IActionResult UnsubscribeFromTopic()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> UnsubscribeFromTopic(string email)
+        {
+            using var snsClient = _awsService.GetSnsAccessClient();
+
+            var subscribers = await snsClient.ListSubscriptionsByTopicAsync(_awsService.GetSnsTopicArn());
+
+            var subscription = subscribers.Subscriptions.FirstOrDefault(x => x.Endpoint == email);
+
+            if (subscription is null)
+            {
+                return NotFound();
+            }
+            
+            var unsubscribeRequest = new UnsubscribeRequest(subscription.SubscriptionArn);
+            var unsubscribeResponse = await snsClient.UnsubscribeAsync(unsubscribeRequest);
+
+            return View(unsubscribeResponse.ResponseMetadata);
         }
     }
 }
